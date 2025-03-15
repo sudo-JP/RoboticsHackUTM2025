7bbba3473a6e11219284954f16261b83a919b652
JaseCodez <135294048+JaseCodez@users.noreply.github.com>
2025-03-15 18:12:30 -0400

    Update HackathonUTM.ino

--- HackathonUTM.ino
+++ HackathonUTM.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_AHTX0.h>
+#include "NewPing.h"
 
 // include TFT and SPI libraries
 #include <TFT.h>  
@@ -8,18 +9,30 @@
 #define cs   10
 #define dc   9
 #define rst  8
-int analogPin = 0; //connect water sensor to analog interface 0
-int water = 0; //define the initial value of variable ‘data’ as 0
-const int led = 0;
+#define MAX_DISTANCE 80
+
+int analogPin = A0; //connect water sensor to analog interface 0
+const int waterSensorPin = 0;
+const int ledPin = 2;
+const int buzzer = 6; 
+const int trigPin = 5; // define ultrasonic signal receiver pin ECHO to D4 
+const int echoPin = 4; 
 
 // Create an instance of the AHT10 sensor
 Adafruit_AHTX0 aht;
 TFT screen = TFT(cs, dc, rst);
 
+NewPing sonar(trigPin, echoPin, MAX_DISTANCE);
+
 void setup() {
   Serial.begin(9600);
   Serial.println("AHT10 test");
-  pinMode(led, OUTPUT); 
+  pinMode(ledPin, OUTPUT); 
+  pinMode(buzzer, OUTPUT); 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT); 
+
+  digitalWrite(ledPin, LOW);
   // Initialize the sensor
   if (!aht.begin()) {
     Serial.println("Could not find AHT10 sensor! Check wiring.");
@@ -34,8 +47,22 @@ void setup() {
 
 }
 
+void detectObject() {
+    if (sonar.ping_cm() <= 20) {
+      // BUZ
+        screen.text("ANIMAL DETECTED", 0, 30); 
+        for(int i=0;i<80*10;i++)// output a frequency sound
+{ digitalWrite(buzzer,HIGH);// sound
+delay(1);//delay1ms 
+digitalWrite(buzzer,LOW);//not sound
+delay(1);//ms delay 
+    //not sound
+  }
+    }
+}
+
 void loop() {
-  water = analogRead(analogPin); 
+  int sensorValue = analogRead(waterSensorPin);
   
   // Read temperature and humidity
   sensors_event_t humidity, temp;
@@ -53,32 +80,31 @@ void loop() {
   // Humidity
   char num_humid[100];
   char strtemp2[64];
-  dtostrf(humidity.relative_humidity, 4, 2, strtemp);
+  dtostrf(humidity.relative_humidity, 4, 2, strtemp2);
 
-  sprintf(num,"Temperature: %s C", strtemp);
-  screen.text(num, 0, 0); 
+  sprintf(num_humid,"Humidity: %s", strtemp2);
+  screen.text(num_humid, 0, 10); 
 
 
-  // Water
-  if (water > 700) {
-    digitalWrite(led, HIGH); 
+  if (sensorValue > 500) {
+    // High water level: Turn on the LED
+    digitalWrite(ledPin, HIGH);
+    screen.text("STOP WATERING YOUR PLANT", 0, 20); 
+  } else if (sensorValue > 300) {
+    // Medium water level: Turn on the LED
+    digitalWrite(ledPin, HIGH);
+    screen.text("YOUR PLANT IS GOOD", 0, 20); 
+    
+  } else {
+    // Low or no water: Turn off the LED
+    digitalWrite(ledPin, LOW);
+    screen.text("WATER YOUR PLANT", 0, 20); 
   }
-  else {
-    digitalWrite(led, LOW); 
-  }
-  // Serial.print("Temperature: "); 
-  // Serial.print(temp.temperature); 
-  // Serial.println(" °C");
+  detectObject(); 
 
-  // Serial.print("Humidity: "); 
-  // Serial.print(humidity.relative_humidity); 
-  // Serial.println(" %");
 
-  delay(5000); // Wait 1 second before the next reading
+  delay(5000); // Wait 5 second before the next reading
   
-  //screen.fill(0,0,0);
-  //screen.rect(-20, -20, 1000, 10000);
   screen.fillScreen(0);
-  
   
 }
